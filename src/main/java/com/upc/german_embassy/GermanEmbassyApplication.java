a849484cb9413bd4e163eca5df84326bbc06eb68
Alejandro José Junior Cruz Mocarro <u202114593@upc.edu.pe>
2023-09-08 19:51:28 -0500

    Initial commit

---
+++ src/main/java/com/upc/german_embassy/GermanEmbassyApplication.java
@@ -0,0 +1,13 @@
+package com.upc.german_embassy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermanEmbassyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermanEmbassyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/german_embassy/GermanEmbassyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.german_embassy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermanEmbassyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
