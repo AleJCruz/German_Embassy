28076281e9d4829dd77b27c3ba0b6f39177f98e7
Alejandro José Junior Cruz Mocarro <144296599+AleJCruz@users.noreply.github.com>
2024-02-01 12:22:43 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Recuperar secretos desde Azure Key Vault
-SECRET1=$(az keyvault secret show --vault-name tu-keyvault --name secret1 --query value --output tsv)
-SECRET2=$(az keyvault secret show --vault-name tu-keyvault --name secret2 --query value --output tsv)
+SECRET1=$(az keyvault secret show --vault-name ExampleAle --name SecretEx  --query value --output tsv)
 
 # Configurar variables de entorno
 export SECRET1
-export SECRET2
 
 # Ejecutar la aplicación Spring Boot
 java -jar app.jar
